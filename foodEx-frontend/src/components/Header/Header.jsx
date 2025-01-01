dce3a4c6c752db2a0dc777bdc1826d64fbc87d16
2MJ <attahiruj@gmail.com>
2025-01-01 08:40:42 +0200

    explore menu implementation

---
+++ foodEx-frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Savor the Flavors of Africa</h2>
+        <p>
+        Order your favorite dishes from our curated menu of authentic African delicacies, 
+        crafted with the freshest ingredients and inspired by the rich culinary traditions of the continent. 
+        From hearty stews to vibrant spices, every bite is a celebration of flavor, culture, and community.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
